c0fcb28515d92710bc674b765e6724ba2e94abe8
AlexFairy <AlexM121687@gmail.com>
2024-08-29 15:13:19 -0700

    add project

--- assignment3.py
+++ assignment3.py
@@ -1,43 +1,43 @@
-# 1. Nested Decisions: The Adventure Game
-#Objective: To practice the use of nested if statements.
-
+"""
 # Task 1: 
 # Code Correction 
-# You are provided with a Python script that is supposed to guide a user through an adventure game, but it has some errors. 
-# Identify and fix them.
-# Buggy Code:
 
-place = input("Choose a place: forest or cave? ")
+place = input("Choose a place: forest or cave? ").lower()
 
 if place == "forest":
-    action = input("climb a tree or cross a river?")
+    action = input("climb a tree or cross a river? ").lower()
     if action == "climb a tree":
         print("You found a bird's nest!")
     elif action == "cross a river":
         print("You found a boat!")
 elif place == "cave":
     print("You find a hidden treasure!")
+else:
+    print("Make a proper selection!")
+
 
 # Task 2: Setting the Scene
-# Based on your corrected code from Task 1, 
-# expand the game. If the user chooses "cave", 
-# ask them if they want to "light a torch" or "proceed in the dark", and provide outcomes for each decision.
 
-place = input("Choose a place: forest or cave? ")
+place = input("Choose a place: forest or cave? ").lower()
 
 if place == "forest":
-    action = input("climb a tree or cross a river?")
-    if action == "climb a tree":
-        print("You found a bird's nest!")
-    elif action == "cross a river":
-        print("You found a boat!")
+  action = input("climb a tree or cross a river? ").lower()
+  if action == "climb a tree":
+      print("You found a bird's nest!")
+  elif action == "cross a river":
+      print("You found a boat!")
+  else:
+      print("Incorrect selection. Try again!")
 elif place == "cave":
-    print("You find a hidden treasure!")
-    action = input("return home and keep it safe! or explore further to seek more treasure ")
-    if action == "return home and keep it safe!":
+  print("You find a hidden treasure!")
+  print("Please either select 1 or 2.")
+  action = input("1. return home and keep it safe! or 2. explore further to seek more treasure: ").lower()
+  if action == "1":
         print("light a tourch")
-    elif action == "exploore further to seek more treasure":
-        print("proceed in the dark")                
+  elif action == "2":
+        print("proceed in the dark")
+  else:
+      print("Incorrect selection. Try again!")                
 
 # Task 3: Default Path
 # If the user makes an invalid choice at any point, 
@@ -45,21 +45,19 @@ elif place == "cave":
 # How can an else statement be of use here?
 
 
-value = 12
+value = int(input("Type a number: "))
 if value > 5:
-  pass
-
+  print(f"The input number is bigger than 5! Good job!")
+elif value < 5:
+  print("The input number is smaller than 5! Good job!")
 else:
-  print("Invalid")
+  pass
+  print(f"Incorrect")
 
-
-# 2. Quick Decisions: The Event Planner
-# Objective: To practice the use of shorthand if statements.
+  
 # Task 1: Code Correction 
-# You are provided with a Python script that is supposed to help in event planning, 
-# but it has errors. Identify and fix them.
+
 # Buggy Code:
-# 
 
 attendees = input("Enter number of attendees: ")
 venue = "large hall" if attendees > 100 else "conference room"
@@ -70,6 +68,7 @@ attendees = int(input("Enter number of attendees: "))
 venue = "large hall" if attendees > 100 else "conference room"
 print(venue)
 
+
 # Task 2: Venue Selection
 
 # Based on the corrected code from Task 1, 
@@ -78,23 +77,22 @@ print(venue)
 attendees = int(input("Enter number of attendees: "))
 venue = "large hall and audio system" if attendees > 100 else "conference room and projector"
 print(venue)
-
+"""
 # Task 3: Catering Choices
 # Ask the user 
 # if they want "vegetarian" food. 
-# Recommend "Veggie Delight Caterers" if yes, otherwise recommend "Gourmet Meals Caterers".
+# Recommend "Veggie Delight Caterers" if yes, otherwise recommend "Gourmet Meals Caterers")
 
-food_choice = input("Interested in vegetarian food? yes/no?: ")
-veggie_meal = input("We do have Veggie Delight Caterers, interested? yes/no: ")
+user_input = input("Interest in vegetarian cuisine?(yes/no) ").lower()
 
-if food_choice == "yes": 
-  if veggie_meal == "yes":
-    print("I highly recommend Gourmet Meals Caterers!")
-  elif veggie_meal == "no":
-    print("We got other options!")
-
+if user_input == 'yes':
+  print("I highly reccomend Veggie Delight Caterers")
+  user_input = input("Interested?(yes/no) ").lower()
+  if user_input  == 'yes':
+    print("We'll get started on your order!")
+  elif user_input == 'no':
+    print("Then I can reccomend Gourtmet Meals Caterers")
+  else:
+    print("Incorrect entry!")
 else:
-    if food_choice == "no":
-      print("We got meat options too!")
-
-#I got stuck in the last coding task, so please write it out so I can see where I made the error. Thanks.
+  print("We do have other options such as meat and fish")
